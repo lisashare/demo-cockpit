5e8bd7215d29c288de8fdff7f5c6d33fb224f6eb
lisashare <lisa_zhu2012@foxmail.com>
2019-12-18 15:47:59 +0800

    first-commit

---
+++ mock/data.js
@@ -0,0 +1,150 @@
+var data = {
+    "map": {
+        "signData":{
+            "北京": [
+                [{
+                    name: '北京'
+                }, {
+                    name: '北京',
+                    value: 140
+                }],
+                [{
+                    name: '北京'
+                }, {
+                    name: '新疆',
+                    value: 9
+                }],
+                [{
+                    name: '北京'
+                }, {
+                    name: '黑龙江',
+                    value: 20
+                }],
+                [{
+                    name: '北京'
+                }, {
+                    name: '辽宁',
+                    value: 90
+                }],
+                [{
+                    name: '北京'
+                }, {
+                    name: '天津',
+                    value: 70
+                }]
+            ],
+            "上海": [
+                [{
+                    name: '上海'
+                }, {
+                    name: '上海',
+                    value: 100
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '湖南',
+                    value: 60
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '河南',
+                    value: 40
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '西藏',
+                    value: 8
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '重庆',
+                    value: 50
+                }]
+            ],
+        },
+        "visitData":{
+            "青海": [
+                [{
+                    name: '青海'
+                }, {
+                    name: '青海',
+                    value: 160
+                }],
+                [{
+                    name: '青海'
+                }, {
+                    name: '新疆',
+                    value: 90
+                }],
+                [{
+                    name: '青海'
+                }, {
+                    name: '广西',
+                    value: 90
+                }],
+                [{
+                    name: '青海'
+                }, {
+                    name: '辽宁',
+                    value: 90
+                }],
+                [{
+                    name: '青海'
+                }, {
+                    name: '天津',
+                    value: 90
+                }]
+            ],
+            "上海": [
+                [{
+                    name: '上海'
+                }, {
+                    name: '上海',
+                    value: 100
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '四川',
+                    value: 80
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '贵州',
+                    value: 60
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '江苏',
+                    value: 4
+                }],
+                [{
+                    name: '上海'
+                }, {
+                    name: '陕西',
+                    value: 50
+                }]
+            ],
+        }
+    },
+    "funnel": [
+            {value: 456, name: '资源量'},
+            {value: 365, name: '邀约量'},
+            {value: 291, name: '来访量'},
+            {value: 231, name: '签约量'}
+        ],
+    "sign": {
+        "signNum": 123 ,
+        "signingNum": 1234567 ,
+        "signAmountNum": 938495.33,
+    }
+}
+
+
+ 
