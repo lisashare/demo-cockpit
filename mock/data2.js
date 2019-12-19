2c0dcf1eb5b5e59b8c82273b6bdec035c0f621b6
lisashare <lisa_zhu2012@foxmail.com>
2019-12-19 10:22:09 +0800

    data

--- mock/data2.js
+++ mock/data2.js
@@ -1,26 +1,26 @@
 
 	var DataList = [{
-		"monthclewconversation": "",
-		"monthresourceamount": "2",
-		"monthinvitationamount": "2",
-		"monthcomeamount": "2",
-		"monthsignamount": "2",
-		"monthsignsingleamount": "2",
-		"monthsignprice": "2.00",
-		"dayresourceamount": "200",
-		"daycallamount": "2",
-		"dayinvitationamount": "2",
-		"daycomeamount": "2",
-		"daysignamount": "2",
-		"daysignsingleamount": "2",
-		"daysignprice": "10200.00",
-		"dayserviceonlinenum": "",
-		"dayconverastionamount": "",
-		"dayaccountconsumer": "",
-		"dayclewconversation": "",
-		"daytotalsignsingleamount": "",
-		"daytotalsignamount": "",
-		"daytotalsignprice": "",
+		"monthclewconversation": "", // 月线索回话
+		"monthresourceamount": "2", // 月资源总数
+		"monthinvitationamount": "2", // 月邀约总数
+		"monthcomeamount": "2", // 月来访量
+		"monthsignamount": "2", // 月签约量
+		"monthsignsingleamount": "2", // 月签约单量
+		"monthsignprice": "2.00", // 月签约金额
+		"dayresourceamount": "200", // 日资源总量
+		"daycallamount": "2", // 日通话总量
+		"dayinvitationamount": "2", // 日邀约量
+		"daycomeamount": "2", // 日来访量
+		"daysignamount": "2", // 日签约量
+		"daysignsingleamount": "2", // 日签约单量
+		"daysignprice": "10200.00", // 日签约金额
+		"dayserviceonlinenum": "", //  日客服在线量
+		"dayconverastionamount": "", // 日会话总量
+		"dayaccountconsumer": "", // 日账户消费
+		"dayclewconversation": "", // 日线索会话
+		"daytotalsignsingleamount": "", // 日签约单量 (中)
+		"daytotalsignamount": "", // 日签约量(中)
+		"daytotalsignprice": "", // 日签约金额(中)
 		"mapcomevisitamount": [{
 			"city": "北京",
 			"num": "100",
@@ -119,7 +119,7 @@
 			"num": "200",
 			"startoff": "广州"
 		}],
-		"fromchan": "2"
+		"fromchan": "2" // 小物种
 	}, {
 		"monthclewconversation": "",
 		"monthresourceamount": "1000",
