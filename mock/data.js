2888ef8684af3160fb67714149a5ac1fbc6f709e
lisashare <lisa_zhu2012@foxmail.com>
2019-12-19 20:47:40 +0800

    联调接口

--- mock/data.js
+++ mock/data.js
@@ -1,178 +1,247 @@
-var data = {
-    "map": {
-        "signData":{
-            "北京": [
-                [
-                {
-                    name: '北京'
-                }, {
-                    name: '北京',
-                    value: ""
-                }
-                ],
-                [{
-                    name: '北京'
-                }, {
-                    name: '上海',
-                    value: 9
-                }],
-                [{
-                    name: '北京'
-                }, {
-                    name: '黑龙江',
-                    value: 20
-                }],
-                [{
-                    name: '北京'
-                }, {
-                    name: '辽宁',
-                    value: 90
-                }],
-                [{
-                    name: '北京'
-                }, {
-                    name: '天津',
-                    value: 70
-                }]
-            ],
-            "新疆": [
-                [{
-                    name: '新疆'
-                }, {
-                    name: '新疆',
-                    value: "0"
-                }],
-                [{
-                    name: '新疆'
-                }, {
-                    name: '北京',
-                    value: "0"
-                }],
-                [{
-                    name: '新疆'
-                }, {
-                    name: '湖北',
-                    value: "110"
-                }]
-            ],
-            "上海": [
-                // [{
-                //     name: '上海'
-                // }, {
-                //     name: '上海',
-                //     value: 1
-                // }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '北京',
-                    value: ""
-                }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '河南',
-                    value: 40
-                }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '西藏',
-                    value: 8
-                }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '重庆',
-                    value: 50
-                }]
-            ],
-        },
-        "visitData":{
-            "青海": [
-                [{
-                    name: '青海'
-                }, {
-                    name: '青海',
-                    value: 160
-                }],
-                [{
-                    name: '青海'
-                }, {
-                    name: '北京',
-                    value: 90
-                }],
-                [{
-                    name: '青海'
-                }, {
-                    name: '广西',
-                    value: 90
-                }],
-                [{
-                    name: '青海'
-                }, {
-                    name: '辽宁',
-                    value: 90
-                }],
-                [{
-                    name: '青海'
-                }, {
-                    name: '天津',
-                    value: 90
-                }]
-            ],
-            "上海": [
-                [{
-                    name: '上海'
-                }, {
-                    name: '上海',
-                    value: 100
-                }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '四川',
-                    value: 80
-                }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '贵州',
-                    value: 60
-                }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '江苏',
-                    value: 4
-                }],
-                [{
-                    name: '上海'
-                }, {
-                    name: '陕西',
-                    value: 50
-                }]
-            ],
-        }
-    },
-    "funnel": [
-            {"value": 456, "name": '资源量'},
-            {"value": 365, "name": '邀约量'},
-            {"value": 291, "name": '来访量'},
-            {"value": 0, "name": '签约量'}
-        ],
-    "sign": {
-        "signNum": 123 ,
-        "signingNum": 1234567 ,
-        "signAmountNum": 938495.33,
-    },
-    "bar": [
-        {"name": "客服在线数","value": 122222},
-        {"name": "会话总量","value": 23344},
-        {"name": "账户消费","value": 26574},
-        {"name": "线索会话","value": 34},
-    ]
-}
 
-
- 
+	var DataList = [
+		{
+		"monthclewconversation": "", // 月线索会话
+		"monthresourceamount": "2", // 月资源总数
+		"monthinvitationamount": "2", // 月邀约总数
+		"monthcomeamount": "2", // 月来访量
+		"monthsignamount": "2", // 月签约量
+		"monthsignsingleamount": "2", // 月签约单量
+		"monthsignprice": "2.00", // 月签约金额
+		"dayresourceamount": "200", // 日资源总量
+		"daycallamount": "2", // 日通话总量
+		"dayinvitationamount": "2", // 日邀约量
+		"daycomeamount": "2", // 日来访量
+		"daysignamount": "2", // 日签约量
+		"daysignsingleamount": "2", // 日签约单量
+		"daysignprice": "10200.00", // 日签约金额
+		"dayserviceonlinenum": "", //  日客服在线量
+		"dayconverastionamount": "", // 日会话总量
+		"dayaccountconsumer": "", // 日账户消费
+		"dayclewconversation": "", // 日线索会话
+		"daytotalsignsingleamount": "", // 日签约单量 (中)
+		"daytotalsignamount": "", // 日签约量(中)
+		"daytotalsignprice": "", // 日签约金额(中)
+		"mapcomevisitamount": [
+			{
+			"city": "北京",
+			"num": "100",
+			"startoff": "深圳"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "深圳"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "深圳"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "北京"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "北京"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "北京"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "上海"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "上海"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "上海"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "广州"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "广州"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "广州"
+		}],
+		"mapsignamount": [{
+			"city": "北京",
+			"num": "100",
+			"startoff": "深圳"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "深圳"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "深圳"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "北京"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "北京"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "北京"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "上海"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "上海"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "上海"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "广州"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "广州"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "广州"
+		}],
+		"fromchan": "2" // 小物种
+	}, {
+		"monthclewconversation": "",
+		"monthresourceamount": "1000",
+		"monthinvitationamount": "1000",
+		"monthcomeamount": "1000",
+		"monthsignamount": "1000",
+		"monthsignsingleamount": "1000",
+		"monthsignprice": "1000.00",
+		"dayresourceamount": "1000",
+		"daycallamount": "1000",
+		"dayinvitationamount": "1000",
+		"daycomeamount": "1000",
+		"daysignamount": "1000",
+		"daysignsingleamount": "1000",
+		"daysignprice": "1000.00",
+		"dayserviceonlinenum": "",
+		"dayconverastionamount": "",
+		"dayaccountconsumer": "",
+		"dayclewconversation": "",
+		"daytotalsignsingleamount": "",
+		"daytotalsignamount": "",
+		"daytotalsignprice": "",
+		"mapcomevisitamount": [{
+			"city": "上海",
+			"num": "200",
+			"startoff": "北京"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "北京"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "北京"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "上海"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "上海"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "上海"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "广州"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "广州"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "广州"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "深圳"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "深圳"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "深圳"
+		}],
+		"mapsignamount": [{
+			"city": "上海",
+			"num": "200",
+			"startoff": "北京"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "北京"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "北京"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "上海"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "上海"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "上海"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "广州"
+		}, {
+			"city": "深圳",
+			"num": "400",
+			"startoff": "广州"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "广州"
+		}, {
+			"city": "北京",
+			"num": "100",
+			"startoff": "深圳"
+		}, {
+			"city": "上海",
+			"num": "200",
+			"startoff": "深圳"
+		}, {
+			"city": "广州",
+			"num": "300",
+			"startoff": "深圳"
+		}],
+		"fromchan": "1"
+	}
+];
